66d2d2e3fe59e8c34593fbc7e8e9bd9750714630
vymat <vymatvienko@gmail.com>
2023-10-06 13:03:24 +0300

    Задание выполнено

---
+++ e2e/form.test.js
@@ -0,0 +1,71 @@
+import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Credit Card Validator form', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('Form should render on page start', async () => {
+    await page.goto(baseUrl);
+
+    await page.waitForSelector('.card-form-widget');
+  });
+
+  test('Form input should add .valid class if card number is valid', async () => {
+    await page.goto(baseUrl);
+
+    await page.waitForSelector('.card-form-widget');
+
+    const form = await page.$('.card-form-widget');
+    const input = await form.$('.input');
+    const submit = await form.$('.submit');
+
+    await input.type('2200150223920999');
+    await submit.click();
+
+    await page.waitForSelector('.card-form-widget .input.valid');
+  });
+
+  test('Form input should add .invalid class if card number is invalid', async () => {
+    await page.goto(baseUrl);
+
+    await page.waitForSelector('.card-form-widget');
+
+    const form = await page.$('.card-form-widget');
+    const input = await form.$('.input');
+    const submit = await form.$('.submit');
+
+    await input.type('2200150223920998');
+    await submit.click();
+
+    await page.waitForSelector('.card-form-widget .input.invalid');
+  });
+});
---
+++ src/js/validators.js
@@ -0,0 +1,45 @@
+export function isValidCard(value) {
+  if (value.length === 0) {
+    return false;
+  }
+
+  let sum = 0;
+  const nDigits = value.length;
+  const parity = nDigits % 2;
+  let digit;
+  for (let i = 0; i < nDigits; i += 1) {
+    digit = Number(value[i]);
+
+    if (i % 2 === parity) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+  }
+  return sum % 10 === 0;
+}
+
+export function getCardType(value) {
+  if (value[0] === '2') {
+    return 'mir';
+  } if (value[0] === '4') {
+    return 'visa';
+  } if (value[0] === '5') {
+    return 'mastercard';
+  } if (value.substr(0, 2) === '34' || value.substr(0, 2) === '37') {
+    return 'amex';
+  } if (value.substr(0, 2) === '60') {
+    return 'discover';
+  } if (value.substr(0, 2) === '31' || value.substr(0, 2) === '35') {
+    return 'jcb';
+  } if (
+    value.substr(0, 2) === '30'
+      || value.substr(0, 2) === '36'
+      || value.substr(0, 2) === '38'
+  ) {
+    return 'diners';
+  }
+  return 'others';
+}
